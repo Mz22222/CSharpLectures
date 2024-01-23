264b59498ad7a413b244df382c2eab972ea458e9
Zara Mirzakhmedova <zara-0512@yandex.ru>
2024-01-24 01:19:27 +0300

    update_1 Lesson_4:Task01

--- Lesson_4/Task1/Program.cs
+++ Lesson_4/Task1/Program.cs
@@ -10,17 +10,17 @@ int[,] CreateMatrix (int rowCount, int columsCount)
 
     int[,] matrix = new int[rowCount, columsCount ];
 
-Random rnd = new Random();
-for (int i = 0; i < matrix.GetLength(0); i++)
-{
-    for (int j = 0; j < matrix.GetLength(1); j++)
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
     {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
 
-        matrix[i, j] = rnd.Next(1, 11);
+            matrix[i, j] = rnd.Next(1, 11);
+        }
     }
-}
-return matrix;
-}
+    return matrix;
+    }
 
 void ShowMatrix(int [,] matrix)
 { 
